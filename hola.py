e70452291d181be6d565fb182ed09476b9489f37
Mincito <borellanas@gmail.com>
2025-04-24 15:35:51 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,77 @@
+
+# print ("ingrese un numero")
+# num=int(input())
+# print ("ingrese otro numero")
+# num2=int(input())
+# print("Su total es: ", num+num2 )
+ 
+# n1=int(input("Ingrese su numero"))
+# n2=int(input("Ingrese su otro numero"))
+# print("El resultado es", n1+n2)
+
+# print("ingrese su nombre")
+# nombre=(input())
+# print("Ingrese su edad")
+# edad=int(input())
+
+# #[print("hola", nombre, "su edad es", edad)]
+# print(f"hola {nombre} su edad es de {edad}")
+
+# if edad>=18:
+#     print ("usted es mayor de edad")
+# else:
+#     print("usted no es mayor de edad")
+
+#menor de 12 es niño
+#entre 12 y 17 es adolescente
+#entre 18 y 64 es adulto
+#65 ymas es adulto mayor
+
+# print("Ingrese su edad")
+# edad=int(input())
+
+# if edad<12:
+#     print ("usted es niño")
+# elif edad>=12 and edad<=18:
+#     print("usted es adolescente")
+
+# elif   edad>=18 and edad<= 64:
+#     print("Usted es adulto")
+# else:
+#     print("Usted es adulto mayor")
+
+
+## Clave secreta
+# # clave=3344
+# print ("Seleccione su clave")
+# pas=int(input())
+# if clave==pas:
+#         print("Clave aceptada")
+# else:
+#         print("Clave incorrecta")
+
+# ## Numeros pares e impares
+# num=int(input("Ingrese un numero"))
+
+# if num %2==0:
+#     print("El numero es par")
+# else:
+#     print ("El numero es impar")
+
+color=input("Ingrese un color")
+
+# si es azul es de la u
+# si es blanco cc
+# si es rojo union
+# si es verde, audax
+# si es otro no es color valido
+
+if color=="azul":
+    print("Es de la u")
+elif color=="blanco":
+    print("Es del colo")
+elif color=="rojo":
+    print("Es de union")
+elif color=="verde":
+    print("es del audax")
+else: "color no valido"
